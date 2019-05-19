9a1d6377c753b3e9d977a0c535482c2a7d447eed
Ruslan <ruslanfilistovich@gmail.com>
2019-05-19 14:44:03 +0300

    упрощение кода

--- things.js
+++ things.js
@@ -1,13 +1,9 @@
-var array_counter = function(array) {
+module.exports.array_counter = function(array) {
 	return "В массиве находиться " + array.length + " элементов!";
 };
 
-var multiply = function(x, y) {
+module.exports.multiply = function(x, y) {
 	return `${x} умножить ${y} равно ${x * y}`;
 };
 
-var some_value = 451;
-
-module.exports.array_counter = array_counter;
-module.exports.multiply = multiply;
-module.exports.some_value = some_value;
+module.exports.some_value = 451;
