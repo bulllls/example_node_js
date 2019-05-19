1018be42876dfc161cf230ea84cb01f95de425f7
Ruslan <ruslanfilistovich@gmail.com>
2019-05-19 14:40:42 +0300

    fix syntax

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var things = require('./things');
 
-console.log("Hello World");
-console.log(__dirname);
-console.log(__filename);
+// console.log("Hello World");
+// console.log(__dirname);
+// console.log(__filename);
 
-// console.log(things.some_value);
-// console.log(things.array_counter([1, 7, 99, 8, 45, 8]));
-// console.log(things.multiply(5, 8));
+console.log(things.some_value);
+console.log(things.array_counter([1, 7, 99, 8, 45, 8]));
+console.log(things.multiply(5, 8));
--- things.js
+++ things.js
@@ -1,9 +1,9 @@
 var array_counter = function(array) {
-	return "В массиве находиться" + array.lenght + "элементов!";
+	return "В массиве находиться " + array.length + " элементов!";
 };
 
 var multiply = function(x, y) {
-	return `$(x) умножить $(y) равно $(x * y)`;
+	return `${x} умножить ${y} равно ${x * y}`;
 };
 
 var some_value = 451;
