33979ad62335d0c8cd12ee3f5c58db6ccc1b2ad7
Ruslan <ruslanfilistovich@gmail.com>
2019-05-19 13:52:50 +0300

    test

---
+++ array.js
@@ -0,0 +1,11 @@
+var array_counter = function(array) {
+	return "В массиве находиться" + array.lenght + "элементов!";
+};
+
+var multiply = function(x, y) {
+	return
+};
+
+var some_value = 451;
+
+module.exports = array_counter;
