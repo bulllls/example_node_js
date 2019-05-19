e3809d7a07dbe0cf87a93ecf5c12553a0740b5e6
Ruslan <ruslanfilistovich@gmail.com>
2019-05-19 14:05:06 +0300

    learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var counter = require('./things');
+var things = require('./things');
 
-console.log("Hello World");
-console.log(__dirname);
-console.log(__filename);
+// console.log("Hello World");
+// console.log(__dirname);
+// console.log(__filename);
 
-
-console.log(counter([1, 7, 99, 8, 45, 8]));
+console.log(things.some_value);
+console.log(things.array_counter([1, 7, 99, 8, 45, 8]));
+console.log(things.multiply(5, 8));
--- things.js
+++ things.js
@@ -9,3 +9,5 @@ var multiply = function(x, y) {
 var some_value = 451;
 
 module.exports.array_counter = array_counter;
+module.exports.multiply = multiply;
+module.exports.some_value = some_value;
