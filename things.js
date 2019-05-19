e872cefe9794d632313b189b4256342cce5123fc
Ruslan <ruslanfilistovich@gmail.com>
2019-05-19 14:47:15 +0300

    3  способ вывода

--- things.js
+++ things.js
@@ -1,9 +1,15 @@
-module.exports.array_counter = function(array) {
+var array_counter = function(array) {
 	return "В массиве находиться " + array.length + " элементов!";
 };
 
-module.exports.multiply = function(x, y) {
+var multiply = function(x, y) {
 	return `${x} умножить ${y} равно ${x * y}`;
 };
 
-module.exports.some_value = 451;
+var some_value = 451;
+
+module.exports = {
+	array_counter: array_counter,
+	multiply: multiply,
+	some_value: some_value
+};
