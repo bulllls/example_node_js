890a2e6b52c534fa742ef35ae99469472851764a
Ruslan <ruslanfilistovich@gmail.com>
2019-05-19 14:14:01 +0300

    test

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var things = require('./things');
 
-// console.log("Hello World");
-// console.log(__dirname);
-// console.log(__filename);
+console.log("Hello World");
+console.log(__dirname);
+console.log(__filename);
 
 console.log(things.some_value);
 console.log(things.array_counter([1, 7, 99, 8, 45, 8]));
